c9cd5a78836107f6d2b57670b151da273f750701
Sophia <sofiyaprdr@gmail.com>
2023-07-21 21:13:58 +0300

    modificated shop app

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const productList = document.getElementById('productList');
 const productInfo = document.getElementById('productInfo');
 const productName = document.getElementById('productName');
 const productsContainer = document.getElementById('products');
+const wrapper = document.getElementById('wrapper');
+const form = document.getElementById('form');
+const submitButton = document.getElementById('submit-btn');
+const fullName = document.getElementById('full-name').value;
+const city = document.getElementById('city').value;
+const postOffice = document.getElementById('post-office').value;
+const postOfficeNumber = parseInt(postOffice);
+const payment = document.querySelector("input[name='payment']:checked").value;
+const productQuantity = document.getElementById('product-quantity').value;
+const userComment = document.getElementById('user-comment').value;
+
+form.style.display = 'none';
 
 const productListTitle = document.createElement('h2');
 productListTitle.textContent = 'Products';
@@ -67,7 +79,7 @@ const displayProductInfo = (e) => {
   const productDescription = getProductDescription(selectedCategory, selectedProduct);
 
   productName.textContent = selectedProduct;
-  productInfo.innerHTML = ''; // Clear existing content
+  productInfo.innerHTML = '';
   productInfo.style.display = 'block';
   productInfo.prepend(productInfoTitle);
   productInfo.append(productDescription);
@@ -76,13 +88,59 @@ const displayProductInfo = (e) => {
 
 productList.addEventListener('click', displayProductInfo);
 
-const resetDefaultState = () => {
-  productInfo.style.display = 'none';
-  productList.innerHTML = '';
-  productListTitle.style.display = 'none';
+// const resetDefaultState = () => {
+//   productInfo.style.display = 'none';
+//   productList.innerHTML = '';
+//   productListTitle.style.display = 'none';
+// };
+
+const displayForm = () => {
+  form.style.display = 'block';
+};
+
+const hideEverything = () => {
+  // productInfo.style.display = 'none';
+  // productList.innerHTML = '';
+  // productListTitle.style.display = 'none';
+  // form.style.display = 'none';
+  wrapper.style.display = 'none';
+};
+
+const displayOrderInfo = () => {
+  const orderInfoContainer = document.createElement('div');
+  const orderSumHeader = document.createElement('h2');
+  const orderSumName = document.createElement('p');
+  const orderSumCity = document.createElement('p');
+  const orderSumPostOffice = document.createElement('p');
+  const orderSumPayment = document.createElement('p');
+  const orderSumProductQuantity = document.createElement('p');
+  const orderSumComment = document.createElement('p');
+
+  orderSumHeader.textContent = 'Thank you for your order! Please, check the data you have provided:';
+  orderSumName.textContent = `Name: ${fullName}`;
+  orderSumCity.textContent = `City: ${city}`;
+  orderSumPostOffice.textContent = `Nova Poshta #: ${postOffice}`;
+  orderSumPayment.textContent = `Payment: ${payment}`;
+  orderSumProductQuantity.textContent = `Quantity of products: ${productQuantity}`;
+  orderSumComment.textContent = `Additional comment: ${userComment}`;
+  document.body.append(orderInfoContainer);
+  orderInfoContainer.append(orderSumHeader, orderSumName, orderSumCity, orderSumPostOffice, orderSumPayment, orderSumProductQuantity, orderSumComment);
 };
 
 buyButton.addEventListener('click', () => {
-  alert('Product purchased!');
-  resetDefaultState();
+  // alert('Product purchased!');
+  // resetDefaultState();
+  displayForm();
 });
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(typeof(postOffice), postOffice);
+  if(!isNaN(postOfficeNumber)) {
+    hideEverything();
+    displayOrderInfo();
+  } else {
+    alert('Please enter Nova Poshta PO number using only numbers.');
+  }
+});
+
